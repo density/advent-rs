52d301c8436706ec2e01becbb47af1ba10e2e724
Justin Prieto <justin@prieto.nyc>
2021-11-26 11:17:57 -0500

    checkpoint

--- 2015/aoc15-06/src/main.rs
+++ 2015/aoc15-06/src/main.rs
@@ -4,31 +4,8 @@ use regex::Regex;
 
 const INPUT: &str = include_str!("../input.txt");
 
-#[derive(Copy, Clone, Eq, PartialEq)]
-enum Light {
-    On,
-    Off,
-}
-
-impl Light {
-    fn turn_on(&mut self) {
-        *self = Light::On;
-    }
-
-    fn turn_off(&mut self) {
-        *self = Light::Off;
-    }
-
-    fn toggle(&mut self) {
-        *self = match self {
-            Light::On => Light::Off,
-            Light::Off => Light::On,
-        }
-    }
-}
-
 fn part1() -> usize {
-    let mut lights = [[Light::Off; 1000]; 1000];
+    let mut lights = [[false; 1000]; 1000];
 
     let re = Regex::new(r"(?P<command>turn off|turn on|toggle) (?P<x1>\d+),(?P<y1>\d+) through (?P<x2>\d+),(?P<y2>\d+)").unwrap();
 
@@ -43,27 +20,46 @@ fn part1() -> usize {
 
         for x in x1..=x2 {
             for y in y1..=y2 {
-                let light = &mut lights[x][y];
-
-                match command {
-                    "turn on" => light.turn_on(),
-                    "turn off" => light.turn_off(),
-                    "toggle" => light.toggle(),
+                lights[x][y] = match command {
+                    "turn on" => true,
+                    "turn off" => false,
+                    "toggle" => !lights[x][y],
                     _ => unreachable!(),
                 }
             }
         }
     }
 
-    lights
-        .iter()
-        .flatten()
-        .filter(|&&light| light == Light::On)
-        .count()
+    lights.iter().flatten().filter(|&&light| light).count()
 }
 
 fn part2() -> u64 {
-    todo!()
+    let mut lights = vec![vec![0u64; 1000]; 1000];
+
+    let re = Regex::new(r"(?P<command>turn off|turn on|toggle) (?P<x1>\d+),(?P<y1>\d+) through (?P<x2>\d+),(?P<y2>\d+)").unwrap();
+
+    for line in INPUT.lines() {
+        let caps = re.captures(line).unwrap();
+
+        let command = &caps["command"];
+        let x1: usize = caps["x1"].parse().unwrap();
+        let y1: usize = caps["y1"].parse().unwrap();
+        let x2: usize = caps["x2"].parse().unwrap();
+        let y2: usize = caps["y2"].parse().unwrap();
+
+        for x in x1..=x2 {
+            for y in y1..=y2 {
+                match command {
+                    "turn on" => lights[x][y] += 1,
+                    "turn off" => lights[x][y] = lights[x][y].saturating_sub(1),
+                    "toggle" => lights[x][y] += 2,
+                    _ => unreachable!(),
+                };
+            }
+        }
+    }
+
+    lights.iter().flatten().sum()
 }
 
 fn main() {
@@ -87,6 +83,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), todo!());
+        assert_eq!(part2(), 15343601);
     }
 }
