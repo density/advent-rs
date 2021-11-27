f1981b4bdc6d98ed42dfd4bb4496683b6fb60c99
Justin Prieto <justin@prieto.nyc>
2021-11-26 20:46:24 -0500

    cleanup

--- 2015/aoc15-06/src/main.rs
+++ 2015/aoc15-06/src/main.rs
@@ -4,23 +4,29 @@ use regex::Regex;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn part1() -> usize {
-    let mut lights = [[false; 1000]; 1000];
-
+fn read_input() -> impl Iterator<Item = (String, usize, usize, usize, usize)> {
     let re = Regex::new(r"(?P<command>turn off|turn on|toggle) (?P<x1>\d+),(?P<y1>\d+) through (?P<x2>\d+),(?P<y2>\d+)").unwrap();
 
-    for line in INPUT.lines() {
+    INPUT.lines().map(move |line| {
         let caps = re.captures(line).unwrap();
 
-        let command = &caps["command"];
+        let command = caps["command"].into();
         let x1: usize = caps["x1"].parse().unwrap();
         let y1: usize = caps["y1"].parse().unwrap();
         let x2: usize = caps["x2"].parse().unwrap();
         let y2: usize = caps["y2"].parse().unwrap();
 
+        (command, x1, y1, x2, y2)
+    })
+}
+
+fn part1() -> usize {
+    let mut lights = vec![vec![false; 1000]; 1000];
+
+    for (command, x1, y1, x2, y2) in read_input() {
         for x in x1..=x2 {
             for y in y1..=y2 {
-                lights[x][y] = match command {
+                lights[x][y] = match command.as_ref() {
                     "turn on" => true,
                     "turn off" => false,
                     "toggle" => !lights[x][y],
@@ -36,23 +42,13 @@ fn part1() -> usize {
 fn part2() -> u64 {
     let mut lights = vec![vec![0u64; 1000]; 1000];
 
-    let re = Regex::new(r"(?P<command>turn off|turn on|toggle) (?P<x1>\d+),(?P<y1>\d+) through (?P<x2>\d+),(?P<y2>\d+)").unwrap();
-
-    for line in INPUT.lines() {
-        let caps = re.captures(line).unwrap();
-
-        let command = &caps["command"];
-        let x1: usize = caps["x1"].parse().unwrap();
-        let y1: usize = caps["y1"].parse().unwrap();
-        let x2: usize = caps["x2"].parse().unwrap();
-        let y2: usize = caps["y2"].parse().unwrap();
-
-        for x in x1..=x2 {
+    for (command, x1, y1, x2, y2) in read_input() {
+        for light_row in lights.iter_mut().take(x2 + 1).skip(x1) {
             for y in y1..=y2 {
-                match command {
-                    "turn on" => lights[x][y] += 1,
-                    "turn off" => lights[x][y] = lights[x][y].saturating_sub(1),
-                    "toggle" => lights[x][y] += 2,
+                match command.as_ref() {
+                    "turn on" => light_row[y] += 1,
+                    "turn off" => light_row[y] = light_row[y].saturating_sub(1),
+                    "toggle" => light_row[y] += 2,
                     _ => unreachable!(),
                 };
             }
