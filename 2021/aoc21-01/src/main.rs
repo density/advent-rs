89b88646c826158266ffc28dd67dc30b745f68f2
Justin Prieto <justin@prieto.nyc>
2021-12-01 00:07:06 -0500

    2021 p1

--- 2021/aoc21-01/src/main.rs
+++ 2021/aoc21-01/src/main.rs
@@ -2,12 +2,21 @@ use std::time::Instant;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn part1() -> u64 {
-    todo!()
+fn part1() -> usize {
+    let ns: Vec<i64> = INPUT.lines().map(|line| line.parse().unwrap()).collect();
+
+    ns.windows(2).filter(|w| w[1] > w[0]).count()
 }
 
-fn part2() -> u64 {
-    todo!()
+fn part2() -> usize {
+    let ns: Vec<u64> = INPUT.lines().map(|line| line.parse().unwrap()).collect();
+
+    let i1 = ns.windows(3);
+    let i2 = ns[1..].windows(3);
+
+    i1.zip(i2)
+        .filter(|(w1, w2)| w2.iter().sum::<u64>() > w1.iter().sum::<u64>())
+        .count()
 }
 
 fn main() {
@@ -26,11 +35,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), todo!());
+        assert_eq!(part1(), 1665);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), todo!());
+        assert_eq!(part2(), 1702);
     }
 }
