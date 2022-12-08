2cf43d35317fab74ed52b7507a1bef13252ed3ce
Justin Prieto <justin@prieto.nyc>
2022-12-07 23:08:13 -0500

    use pathbuf

--- 2022/aoc22-07/src/main.rs
+++ 2022/aoc22-07/src/main.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 use hymns::runner::timed_run;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn calculate_sizes() -> HashMap<String, usize> {
-    let mut dir_sizes: HashMap<String, usize> = HashMap::new();
-    let mut dir_stack = vec!["/"];
+fn calculate_sizes() -> HashMap<PathBuf, usize> {
+    let mut dir_sizes: HashMap<PathBuf, usize> = HashMap::new();
+    let mut cur_path = PathBuf::new();
 
     for line in INPUT.lines() {
         let mut word_iter = line.split_ascii_whitespace();
@@ -15,25 +16,19 @@ fn calculate_sizes() -> HashMap<String, usize> {
             "$" => {
                 if word_iter.next().unwrap() == "cd" {
                     match word_iter.next().unwrap() {
-                        "/" => dir_stack.truncate(1),
+                        "/" => cur_path.push("/"),
                         ".." => {
-                            if dir_stack.len() > 1 {
-                                dir_stack.pop();
-                            }
+                            cur_path.pop();
                         }
-                        other => dir_stack.push(other),
+                        other => cur_path.push(other),
                     }
                 }
             }
             word if word != "dir" => {
                 let size: usize = word.parse().unwrap();
 
-                let mut dir_string = String::new();
-
-                for dir in dir_stack.iter() {
-                    dir_string.push_str(dir);
-                    *dir_sizes.entry(dir_string.clone()).or_insert(0) += size;
-                    dir_string.push('/');
+                for path in cur_path.ancestors() {
+                    *dir_sizes.entry(path.into()).or_insert(0) += size;
                 }
             }
             _ => (),
@@ -50,7 +45,7 @@ fn part1() -> usize {
 
 fn part2() -> usize {
     let dir_sizes = calculate_sizes();
-    let need = 30_000_000 - (70_000_000 - dir_sizes["/"]);
+    let need = 30_000_000 - (70_000_000 - dir_sizes[&PathBuf::from("/")]);
     dir_sizes
         .into_values()
         .filter(|&s| s >= need)
