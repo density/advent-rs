daee5653150ed7645d21742956a889d295d220ed
Justin Prieto <justin@prieto.nyc>
2023-11-30 18:20:18 -0500

    create 2023 p1 skeleton

---
+++ 2023/aoc23-01/src/main.rs
@@ -0,0 +1,31 @@
+use hymns::runner::timed_run;
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn part1() -> u64 {
+    todo!()
+}
+
+fn part2() -> u64 {
+    todo!()
+}
+
+fn main() {
+    timed_run(1, part1);
+    timed_run(2, part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), todo!());
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), todo!());
+    }
+}
