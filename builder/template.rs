4772e49cd75eb248dfaa226466448d8565c8d32f
Justin Prieto <justin@prieto.nyc>
2023-12-01 11:02:35 -0500

    tweak template

--- builder/template.rs
+++ builder/template.rs
@@ -2,12 +2,12 @@ use hymns::runner::timed_run;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn part1() -> u64 {
-    todo!()
+fn part1() -> String {
+    "TODO".to_string()
 }
 
-fn part2() -> u64 {
-    todo!()
+fn part2() -> String {
+    "TODO".to_string()
 }
 
 fn main() {
@@ -21,11 +21,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), todo!());
+        assert_eq!(part1(), "".to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), todo!());
+        assert_eq!(part2(), "".to_string());
     }
 }
