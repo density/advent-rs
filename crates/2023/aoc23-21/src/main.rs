a8c6df413c41ec967526bf9d0ef36af3c37e0888
Justin Prieto <justin@prieto.nyc>
2023-12-22 09:57:02 -0500

    wip

---
+++ crates/2023/aoc23-21/src/main.rs
@@ -0,0 +1,89 @@
+use crate::Plot::{Empty, Rock, Start};
+use hymns::grid::Grid;
+use hymns::runner::timed_run;
+use std::collections::HashSet;
+use std::fmt::{Display, Formatter};
+
+const INPUT: &str = include_str!("../input.txt");
+
+#[derive(Debug, Eq, PartialEq)]
+enum Plot {
+    Empty,
+    Rock,
+    Start,
+}
+
+impl From<char> for Plot {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Empty,
+            '#' => Rock,
+            'S' => Start,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Display for Plot {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Empty => '.',
+            Rock => '#',
+            Start => 'S',
+        };
+
+        write!(f, "{}", c)
+    }
+}
+
+fn part1() -> u64 {
+    let mut grid: Grid<Plot> = INPUT.parse().unwrap();
+
+    let mut loc = grid.iter_points().find(|p| grid[p] == Start).unwrap();
+    grid[loc] = Empty;
+
+    let mut locs = HashSet::from([loc]);
+
+    for step in 0..64 {
+        let mut next_locs = HashSet::new();
+
+        for cur_loc in locs.drain() {
+            let open_neigh = grid
+                .all_neighbors(&cur_loc, false)
+                .into_iter()
+                .filter(|p| grid[p] == Empty);
+
+            next_locs.extend(open_neigh);
+        }
+
+        locs = next_locs;
+    }
+
+    dbg!(locs.len());
+
+    0
+}
+
+fn part2() -> u64 {
+    0
+}
+
+fn main() {
+    timed_run(1, part1);
+    timed_run(2, part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 0);
+    }
+}
